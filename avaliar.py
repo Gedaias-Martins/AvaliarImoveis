3193f376aeead6fcc18b120e016d0771f0231508
Gedaias Martins <56151815+Gedaias-Martins@users.noreply.github.com>
2023-07-20 15:35:11 -0300

    Atualização1

---
+++ avaliar.py
@@ -0,0 +1,15 @@
+casa = int
+apart = int
+
+rj = 5.102 #Valor do metro quadrado no Rio de Janeiro com excessão da Zona Sul
+
+print("Código Avaliador de Imóveis")
+print("INFORME O ENDEREÇO DO IMÓVEL")
+Estado=(input ("ESTADO: "))
+Municipio=(input ("MUNICIPIO: "))
+Bairro=(input("BAIRRO: "))
+print("TIPO DE IMÓVEL")
+larg=int(input("INFORME A LARGURA: "))
+comp=int("INFORME O COMPRIMENTO: ")
+
+print("NuMERO DE QUARTOS: ")
