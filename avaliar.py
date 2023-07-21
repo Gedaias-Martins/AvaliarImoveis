b2978e80e8c7e10d2287682763ffcd4c0e667809
Gedaias Martins <56151815+Gedaias-Martins@users.noreply.github.com>
2023-07-21 09:52:17 -0300

    Atualização
    Localização, Tipo do imóvel.
    Calculo de medidas.

--- avaliar.py
+++ avaliar.py
@@ -1,15 +1,22 @@
 casa = int
 apart = int
+quarto = int
+garage = int
+tImovel = int
 
 rj = 5.102 #Valor do metro quadrado no Rio de Janeiro com excessão da Zona Sul
 
 print("Código Avaliador de Imóveis")
-print("INFORME O ENDEREÇO DO IMÓVEL")
+print("***INFORME O ENDEREÇO DO IMÓVEL***")
 Estado=(input ("ESTADO: "))
 Municipio=(input ("MUNICIPIO: "))
 Bairro=(input("BAIRRO: "))
-print("TIPO DE IMÓVEL")
-larg=int(input("INFORME A LARGURA: "))
-comp=int("INFORME O COMPRIMENTO: ")
+tImovel=(input("TIPO DO IMÓVEL: "))
+lFrente=int(input("INFORME A LARGURA DA FRENTE: "))
+lFundos=int(input("INFORME A LARGURA DE FUNDOS: "))
+comp=int(input("INFORME O COMPRIMENTO: "))
 
-print("NuMERO DE QUARTOS: ")
+print("A LOCALIZAÇÃO DO IMÓVEL: ")
+print( "***LOCALIZAÇÃO*** ", Estado, Municipio, Bairro)
+print("O TIPO DE IMÓVEL: ", tImovel)
+print("MEDIDAS DO IMÓVEL: ", (lFrente + lFundos*comp))
