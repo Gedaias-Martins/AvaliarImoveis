12d5c47f1953a0d47429591e34d456e2864336dc
Gedaias Martins <56151815+Gedaias-Martins@users.noreply.github.com>
2023-10-10 12:59:31 -0300

    Valor dos Imóveis em Belford Roxo
    Foi feita a declaração das variáveis dos imóveis em Belford Roxo - RJ.

--- avaliar.py
+++ avaliar.py
@@ -72,88 +72,87 @@ Machado = 380.000
 MeuCantinho = 70.000
 NovaAurora = 130.000
 #Segunda parte de Cadastro de imóveis em Belford Roxo 
-Nova Piam
-Nossa Senhora do Carmo
-Parque Alvorada
-Parque Americano
-Parque Amorim
-Parque Boa-Sorte
-Parque das Fontes
-Parque dos Ferreiras
-Parque Aida
-Parque Esperança
-Parque Floresta
-Parque Fluminense
-Parque Itambé
-Parque Jordão
-Parque Jupirangai
-Parque Maringá
-Parque Nossa Senhora Aparecida
-Parque Nossa Senhora da Paz
-Parque Ouvidor
-Parque Real
-Parque das Flores
-Parque Santa
-Parque Santa Rita
-Parque São Bento
-Parque São José
-Parque Pica Pau
-Parque São Vicente
-Parque Suécia
-Parque Umari
-Parque União
-Parque Veneza
-Piam (Vila Medeiros)
-Primus
-Ponto 5
-Ponto 2
-Prata
-Recantus (ex Babi)
-Ribaslândia
-Shangri-la
-Sicelândia
-Santa Tereza
-São Bernardo
-São Francisco de Assis
-São Geraldo
-São Jorge
-São Leopoldo
-São Lucas
-Santa Amélia
-Santa Cecília
-Santa Helena
-Santa Maria
-Santa Mônica
-Santo Antônio da Prata
-Santo Reis
-Sargento Roncalli
-Sítio do Livramento
-Sítio Retiro Feliz
-Tamoios
-Três Setas
-Vale das Mangueiras
-Vale do Ipê
-Valparaíso
-Vergél dos Félix
-Vila Palmares
-Vila Pauline
-Vila Rica
-Vila Sagres
-Vila Santa Mercedes
-Vila Santa Rita
-Vila Santa Teresa
-Vila Santo Antônio
-Vila Santo Antônio da Prata
-Vila São Sebastião
-Vila São Sebastião de Nova Aurora
-Vila Seabra
-Vila Verde
-Vila Vitório
-Vilage da Emancipação
-Vilar Novo
-Wona
-Parque colonial
-Parque amorim
+NovaPiam = 300.000
+NossaSenhoradoCarmo = 200.000
+ParqueAlvorada = 79.799
+ParqueAmorim = 125.000
+ParqueBoaSorte = 358.000
+ParquedasFontes = 185.000
+ParquedosFerreiras = 160.000
+ParqueAida = 70.000
+ParqueEsperança = 165.000
+ParqueFloresta = 85.000
+ParqueFluminense = 160.000
+ParqueItambé = 70.000
+ParqueJordão = 70.000
+ParqueJupirangai = 125.000
+ParqueMaringá = 125.000
+ParqueNossaSenhoraAparecida = 70.000
+ParqueNossaSenhoradaPaz = 70.000 
+ParqueOuvidor = 70.000
+ParqueReal = 134.900
+ParquedasFlores = 165.000
+ParqueSanta = 164.200
+ParqueSantaRita = 70.000
+ParqueSãoBento = 320.000 
+ParqueSãoJosé = 125.000
+ParquePicaPau = 85.000
+ParqueSãoVicente = 320.000
+ParqueSuécia = 29.999
+ParqueUmari = 165.000 
+ParqueUnião = 70.000
+ParqueVeneza = 80.000
+Piam = 94.967
+Primus = 165.000
+Ponto5 = 95.000
+Ponto2 = 160.000
+Prata = 85.000
+Recantus = 180.000
+Ribaslândia = 85.000
+Shangrila = 250.000
+Sicelândia = 65.000
+SantaTereza = 125.000
+SãoBernardo = 79.840
+SãoFranciscodeAssis = 145.000
+SãoGeraldo = 70.000
+SãoJorge = 130.000
+SãoLeopoldo = 70.000
+SãoLucas = 85.000 
+SantaAmélia = 123.000
+SantaCecília = 160.000
+SantaHelena = 160.000
+SantaMaria = 120.000
+SantaMônica = 68.620
+SantoAntôniodaPrata = 150.000
+SantoReis = 198.000
+SargentoRoncalli = 107.500
+SítiodoLivramento = 85.000
+SítioRetiroFeliz =  75.798,95
+Tamoios = 159.000
+TrêsSetas = 70.000
+ValedasMangueiras = 70.000
+ValedoIpê = 89.900
+Valparaíso = 60.000
+VergéldosFélix = 192.000
+VilaPalmares = 70.000
+VilaPauline = 195.000
+VilaRica = 70.000
+VilaSagres = 310.000
+VilaSantaMercedes = 165.000
+VilaSantaRita = 250.000
+VilaSantaTeresa = 200.000
+VilaSantoAntônio = 270.000
+VilaSantoAntôniodaPrata = 249.641
+VilaSãoSebastião = 165.000
+VilaSãoSebastiãodeNovaAurora = 180.000
+VilaSeabra = 70.000 
+VilaVerde = 170.000
+VilaVitório = 94.637
+VilagedaEmancipação = 192.000
+VilarNovo = 250.000
+Wona = 400.000
+Parquecolonial = 120.000
+Parqueamorim = 125.000
 
 rj = 5.102 #Valor do metro quadrado no Rio de Janeiro com excessão da Zona Sul
 broxo=int(input(""))
